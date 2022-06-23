212e324dde2ed45a4f69a13c2fc8f66c890d34d1
ᴄyʙᴇʀ_ᴅᴀʀᴋ <89356302+CYBER-D-A-R-K@users.noreply.github.com>
2022-06-23 17:46:37 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+clear
+echo ""
+echo ""
+termux-setup-storage -y
+apt install figlet -y
+apt install ruby -y
+gem install lolcat -y
+clear
+git clone https://github.com/RED5PY/RDS-figlet >> /dev/null 2>&1
+clear
+exit
